e313663d024beff1cd83cb0b5aef7388801aa46f
Fowzy S <fwzsas@gmail.com>
2021-09-08 19:37:28 -0500

    UPDATE

--- Attends/090821/Employee/Main.java
+++ Attends/090821/Employee/Main.java
@@ -2,20 +2,20 @@
 class Main {
     public static void main(String[] args) {
         Employee newEmployee1= new Employee("John");
-        newEmployee1.set_age(44);
-        newEmployee1.set_salary(50000);
+        newEmployee1.set_age(20);
+        newEmployee1.set_salary(10000);
         newEmployee1.set_designation("Programmer");
         System.out.println("First employee infos are: ");
         System.out.println("Name: "+ newEmployee1.get_name());
         System.out.println("Age: "+ newEmployee1.get_age());
-        System.out.println("Age: "+ newEmployee1.get_designation());
+        System.out.println("Designation: "+ newEmployee1.get_designation());
         System.out.println("Salary: "+ newEmployee1.get_salary());
 
-        Employee newEmployee2 = new Employee("Fowzy", 25, "Boss", 100000);
+        Employee newEmployee2 = new Employee("Fowzy", 25, "Your Boss", 100000);
         System.out.println("Second employee infos are: ");
         System.out.println("Name: "+ newEmployee2.get_name());
         System.out.println("Age: "+ newEmployee2.get_age());
-        System.out.println("Age: "+ newEmployee2.get_designation());
+        System.out.println("Designation: "+ newEmployee2.get_designation());
         System.out.println("Salary: "+ newEmployee2.get_salary());
 
     }
